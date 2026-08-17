8e54cd30236a28d30f86b734d91e440596310b00
Hylton Walters <hylton@inqaku.com>
2026-08-17 21:53:06 +0200

    fix: use Bootstrap 5 pagination views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Channel;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot()
   {
+    Paginator::useBootstrapFive();
+
     View::composer('*', function ($view) {
       $view->with('channels', Channel::all());
     });
